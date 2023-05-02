b295cbc272c7ed9cf94ad01b6774b08778935ec1
Fiorela <fazahuanchef@gmail.com>
2023-05-02 15:17:58 -0500

    first commit

---
+++ app/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <Page>
+    <ActionBar title="Listado" />
+    <StackLayout>
+      <ListView for="item in items">
+        <v-template>
+          <StackLayout class="item">
+            <Label :text="item.title" class="title" />
+            <Label :text="item.description" class="description" />
+          </StackLayout>
+        </v-template>
+      </ListView>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data;
+      });
+  },
+};
+</script>
